4aae24dc3c81c95b2cdd1b58dc11d6e3ab97c36a
mfsoom <udu383u3ueue@gmail.com>
2024-07-03 05:07:22 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID", 19645327))
-API_HASH = getenv("API_HASH", "92de937beb2f87db08df95bcca0ac2d6")
+API_ID = int(getenv("API_ID", 25442050))
+API_HASH = getenv("API_HASH", "e81d29a55a67bb6357c9b8af7b769f9b")
 
 BOT_TOKEN = getenv("BOT_TOKEN", "6437907769:AAEgVIwRLzPPcoWZvTn8cFgkR1stTrMVJBM")
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://mohammadtaleb2012t:hamoda7@cluster0.xwc4uff.mongodb.net/?retryWrites=true&w=majority")
