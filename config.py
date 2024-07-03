601a8467ac9e3b120e54db0c28f7b419e2e32bed
mfsoom <udu383u3ueue@gmail.com>
2024-07-03 05:50:18 +0300

    Update config.py

--- config.py
+++ config.py
@@ -12,4 +12,4 @@ BOT_TOKEN = getenv("BOT_TOKEN", "6437907769:AAEgVIwRLzPPcoWZvTn8cFgkR1stTrMVJBM"
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://mohammadtaleb2012t:hamoda7@cluster0.xwc4uff.mongodb.net/?retryWrites=true&w=majority")
 
 OWNER_ID = int(getenv("OWNER_ID", 7190917175))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/A_0_0_0")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/UUUJD")
